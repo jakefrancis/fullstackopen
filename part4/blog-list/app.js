f2fcdf207db318062adbd5b903f3b6f214f4d797
fluffmickgruff <jake.daniel.francis@gmail.com>
2020-09-27 16:42:17 -0400

    extracted routing logic from app.js to controllers/blogs.js

--- part4/blog-list/app.js
+++ part4/blog-list/app.js
@@ -1,33 +1,20 @@
-const express = require('express')
 const config = require('./utils/config')
+const express = require('express')
 const app = express()
-const Blog = require('./models/blog')
 const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
 const mongoose = require('mongoose')
+
 
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
 
+
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 
 module.exports = app
