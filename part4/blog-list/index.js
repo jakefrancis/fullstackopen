b9914e74c2d922649d96dbbdce1d4a5565f49237
fluffmickgruff <jake.daniel.francis@gmail.com>
2020-09-27 16:25:31 -0400

    changed app.listen to server.listen in index.js

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,9 +1,11 @@
 const http = require('http')
 const logger = require('./utils/logger')
 const app = require('./app')
+
 const server = http.createServer(app)
 
 const PORT = process.env.PORT
-app.listen(PORT, () => {
+
+server.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
