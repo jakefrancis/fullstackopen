019d950eae3ae66f73947e1a1919f2bf60914ebf
fluffmickgruff <jake.daniel.francis@gmail.com>
2020-09-27 16:24:18 -0400

    extracted application logic from index.js to app.js

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,39 +1,9 @@
 const http = require('http')
-const Blog = require('./models/blog')
 const logger = require('./utils/logger')
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const app = require('./app')
+const server = http.createServer(app)
 
-
-
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-
-app.use(cors())
-app.use(express.json())
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = 3003
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   logger.info(`Server running on port ${PORT}`)
 })
