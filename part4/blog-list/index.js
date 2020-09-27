883b26ae574e144c9284e22fcaee618cbd53474c
fluffmickgruff <jake.daniel.francis@gmail.com>
2020-09-27 16:15:53 -0400

    extracted mongoose blog schema into models/blog.js

--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,4 +1,5 @@
 const http = require('http')
+const Blog = require('./models/blog')
 const logger = require('./utils/logger')
 require('dotenv').config()
 const express = require('express')
@@ -6,14 +7,7 @@ const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
 
 const mongoUrl = process.env.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
