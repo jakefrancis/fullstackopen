e4306866efbb70708a1223375c67503f0c1b545c
fluffmickgruff <jake.daniel.francis@gmail.com>
2020-09-27 18:03:29 -0400

    created totaLikes() in list_helper.js created totalLike.test.js all tests passed

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+
+  let likes = blogs.map(blog => blog.likes)
+
+  const reducer = (sum , likes) => {
+    return sum + likes
+  }
+  return blogs === 0
+  ? 0
+  : (likes.reduce(reducer, 0))
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
