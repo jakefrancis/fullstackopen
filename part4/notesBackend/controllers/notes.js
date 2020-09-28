307ff08ddca469a35b334a37b3aa8ee87b6cb9e3
fluffmickgruff <jake.daniel.francis@gmail.com>
2020-09-28 19:38:28 -0400

    Refactor DELETE /:id to async/await  in controllers/notes.js

--- part4/notesBackend/controllers/notes.js
+++ part4/notesBackend/controllers/notes.js
@@ -36,12 +36,14 @@ notesRouter.post('/', async (request, response, next) => {
 
 })
 
-notesRouter.delete('/:id', (request, response, next) => {
-  Note.findByIdAndRemove(request.params.id)
-    .then(() => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
+notesRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Note.findByIdAndRemove(request.params.id)
+    response.status(204).end()
+  } catch (exception) {
+    next(exception)
+  }
+
 })
 
 notesRouter.put('/:id', (request, response, next) => {
