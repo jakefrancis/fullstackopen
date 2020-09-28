57857b97d286390951fa4f9035368f27296d4442
fluffmickgruff <jake.daniel.francis@gmail.com>
2020-09-27 20:59:52 -0400

    added lodash as dependency, mostBlogs() added to list_helper.js, created mostBlogs.test.js

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,3 +1,6 @@
+const { result } = require('lodash')
+const _ = require('lodash')
+
 const dummy = (blogs) => {
   return 1
 }
@@ -36,8 +39,60 @@ const favoriteBlog = (blogs) => {
 
 }
 
+/*const mostBlogs = (blogs) => {
+  let authors = {}
+  let mostBlogs = {}
+
+  blogs.forEach( blog => {
+    if(authors[blog.author]){
+        authors[blog.author].blogs++
+    }
+    else{
+      authors[blog.author] = {
+        author: blog.author,
+        blogs: 1
+      }
+    }
+  })
+
+    let greatest = 0
+    for (author in authors)
+    {
+        if(authors[author].blogs > greatest){
+          greatest = authors[author].blogs
+          mostBlogs = {
+            author : authors[author].author,
+            blogs : authors[author].blogs
+          }
+        }
+    }
+
+  return mostBlogs
+
+
+}
+
+*/
+
+const mostBlogs = (blogs) => {
+  
+let authors =  _.countBy(blogs, (blog) => blog.author)
+
+  authors = _.transform(authors, (result, value, key) => {
+    result.push({
+      author: key,
+      blogs : value
+    })
+  }, [])
+
+  return _.maxBy(authors, (author) => author.blogs )
+
+}
+  
+
 module.exports = {
   dummy,
   totalLikes,
   favoriteBlog,
+  mostBlogs,
 }
