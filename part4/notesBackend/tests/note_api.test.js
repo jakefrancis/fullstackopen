bb77c38bea42e2ca8cfe2cf7d4c98bcd79f2fd81
fluffmickgruff <jake.daniel.francis@gmail.com>
2020-09-28 20:10:05 -0400

    Alter beforeEach() to build DB with async from any number of notes

--- part4/notesBackend/tests/note_api.test.js
+++ part4/notesBackend/tests/note_api.test.js
@@ -9,14 +9,24 @@ const Note = require('../models/note')
 beforeEach(async () => {
   await Note.deleteMany({})
 
-  let noteObject = new Note(helper.initialNotes[0])
-  await noteObject.save()
+  //resolves in the correct order
+  for(let note of helper.initialNotes){
+    let noteObject = new Note(note)
+    await noteObject.save()
+  }
 
-  noteObject = new Note(helper.initialNotes[1])
-  await noteObject.save()
+  //performs operations in parralell may change order of DB
+  /*
+  const noteObjects = helper.initialNotes
+    .map(note => new Note(note))
+  const promiseArray = noteObjects.map(note => note.save())
+  await Promise.all(promiseArray)
+  */
+
 })
 
 test('notes are returned as json', async () => {
+  console.log('entered test')
   await api
     .get('/api/notes')
     .expect(200)
